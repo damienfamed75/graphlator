9c90665df2e8e58aa0ac9da17a82e84dd627a5cd
Damien Stamates <thedoubledimensions@gmail.com>
2019-09-23 18:04:40 -0500

    Added a few comments. My typing fingers are tired

--- language.go
+++ language.go
@@ -1,8 +1,15 @@
 package graphlator
 
+// Language determines what kind of result you will get back from the translator.
 type Language int8
 
 const (
+	// GraphQL assigns the translator to turn structured formatted functions
+	// into a GraphQL string query.
+	// Note: This functionality is not begun.
 	GraphQL Language = iota
+	// GraphQLPlus assigned the translator to output Dgraph's GraphQL+- language
+	// for its string queries.
+	// Note: This functionality is not yet finished.
 	GraphQLPlus
 )
--- option.go
+++ option.go
@@ -4,8 +4,12 @@ type option struct {
 	language Language
 }
 
+// Regulation is the type name for any augmentations that are made to the
+// translator.
 type Regulation func(o *option)
 
+// WithLanguage changes the mode that the translator is in.
+// This can be either GraphQL or GraphQLPlus
 func WithLanguage(l Language) Regulation {
 	return func(o *option) {
 		o.language = l
